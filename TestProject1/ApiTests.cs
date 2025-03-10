4351b3287acaf06ce7694a230bb93c6d8c717895
Rajdip27 <rajdip.piistech@gmail.com>
2025-03-10 09:41:11 +0600

    git init

---
+++ TestProject1/ApiTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApplication1;
+using WebApplication1.Controllers;
+using Xunit;
+namespace TestProject1;
+
+public class ApiTests
+{
+    [Fact]
+    public void Get_Returns_OkObjectResult_With_WeatherForecastArray()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<WeatherForecastController>>();
+        var controller = new WeatherForecastController(mockLogger.Object);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result); 
+        Assert.IsType<WeatherForecast[]>(okResult.Value); 
+    }
+
+}
